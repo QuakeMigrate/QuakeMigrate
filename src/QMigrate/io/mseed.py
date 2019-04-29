40e9214ed64b0c272250efbc5fa53609f31b3616
Hemmelig <conor.bacon@gmail.com>
2019-04-29 12:01:48 +0100

    Moved from _cleanup() to merge(method=-1) for stability. Using generators for file lookup.

--- src/QMigrate/io/mseed.py
+++ src/QMigrate/io/mseed.py
@@ -6,35 +6,57 @@ Provides information on station in a given archive.
 
 """
 
-import obspy
 import pathlib
-import QMigrate.core.model as cmod
+from itertools import chain
+
+import obspy
 import numpy as np
 
+import QMigrate.core.model as cmod
 
-class MSEED():
+
+class MSEED(object):
     """
-    MSEED object
+    mSEED object
 
-    Contains
+    Reads data from archive, performs some clean-up and removes any gappy
+    recordings.
 
     Attributes
     ----------
-    MSEED_path : str
-        String describing location of mSEED files
+    MSEED_path : pathlib Path object
+        Location of raw mSEED files
     format : str
         File naming format of data archive
+    signal : 
+
+    filtered_signal : 
+
+    stations : pandas Series object
+        Series object containing station names
+    st : obspy Stream object
+        List like object containing available traces for given stations
+    resample : bool, optional
 
     Methods
     -------
     path_structure(path_type="YEAR/JD/STATION")
         Set the file naming format of the data archive
+    read_mseed(start_time, end_time, sampling_rate)
+        Read in mSEED data between two times
 
     """
 
-    def __init__(self, LUT, HOST_PATH='/PATH/MSEED'):
+    def __init__(self, LUT, HOST_PATH):
         """
         MSEED object initialisation
+
+        Parameters
+        ----------
+        LUT : array-like
+            Lookup table object (only used for station names)
+        HOST_PATH : str
+            Location of raw mSEED files
 
         """
 
@@ -52,13 +74,14 @@ class MSEED():
         del lut
         self.st = None
 
-    def path_structure(self, path_type='YEAR/JD/STATION'):
+    def path_structure(self, path_type="YEAR/JD/STATION"):
         """
         Define the format of the data archive
 
         Parameters
         ----------
-        path_type : str
+        path_type : str, optional
+            Sets path type for different archive formats
 
         """
 
@@ -66,7 +89,7 @@ class MSEED():
             self.format = "{year}/*/{station}/*/*.{station}..*.D.{year}.{jday}"
         elif path_type == "YEAR/JD/STATION":
             self.format = "{year}/{jday}/*_{station}_*"
-        elif path_type == 'STATION.YEAR.JULIANDAY':
+        elif path_type == "STATION.YEAR.JULIANDAY":
             self.format = "*{station}.*.{year}.{jday}"
         elif path_type == "/STATION/STATION.YearMonthDay":
             self.format = "{station}/{station}.{year}{month:02d}{day:02d}"
@@ -87,6 +110,7 @@ class MSEED():
             Sampling rate in hertz
 
         """
+
         self.sampling_rate = sampling_rate
         self.start_time = start_time
         self.end_time = end_time
@@ -94,8 +118,7 @@ class MSEED():
         files = self._load_from_path()
 
         st = obspy.Stream()
-
-        if files:
+        try:
             for file in files:
                 file = str(file)
                 try:
@@ -106,24 +129,24 @@ class MSEED():
                     print(msg.format(file))
                     continue
 
-                # Remove all stations with data gaps greater than
-                # 10.0 milliseconds
-                st._cleanup()
-                gaps = st.get_gaps()
-                if len(gaps) > 0:
-                    stats_with_gaps = np.unique(np.array(gaps)[:, 1]).tolist()
-                    for station in stats_with_gaps:
-                        traces = st.select(station=station)
-                        for trace in traces:
-                            st.remove(trace)
+            # Remove all stations with data gaps greater than
+            # 10.0 milliseconds
+            st.merge(method=-1)
+            gaps = st.get_gaps()
+            if gaps:
+                stations = np.unique(np.array(gaps)[:, 1]).tolist()
+                for station in stations:
+                    traces = st.select(station=station)
+                    for trace in traces:
+                        st.remove(trace)
 
-                # Combining the mseed and determining station availability
-                st.detrend("linear")
-                st.detrend("demean")
-                st = self._downsample(st, sampling_rate)
+            # Combining the mseed and determining station availability
+            st.detrend("linear")
+            st.detrend("demean")
+            st = self._downsample(st, sampling_rate)
 
-                signal, availability = self._station_availability(st, samples)
-        else:
+            signal, availability = self._station_availability(st, samples)
+        except StopIteration:
             print("No data exist for this time period - creating blank")
             availability = np.zeros((len(self.stations), 1))
             signal = np.zeros((3, len(self.stations), int(samples)))
@@ -141,7 +164,7 @@ class MSEED():
         Parameters
         ----------
         stream : obspy Stream object
-
+            Stream containing 3-component trace data for stations
         samples : int
             Number of samples expected in the signal
 
@@ -150,7 +173,7 @@ class MSEED():
         signal : array-like
 
         availability : array-like
-
+            Array containing 0s (no data) or 1s (data)
 
         """
 
@@ -164,18 +187,18 @@ class MSEED():
                         tmp_st[1].stats.npts == samples and
                         tmp_st[2].stats.npts == samples):
 
-                    # Defining the station as avaliable
+                    # Defining the station as available
                     availability[i] = 1
 
                     for tr in tmp_st:
                         channel = tr.stats.channel[-1]
-                        if channel == 'E' or channel == '2':
+                        if channel == "E" or channel == "2":
                             signal[1, i, :] = tr.data
 
-                        if channel == 'N' or channel == '1':
+                        if channel == "N" or channel == "1":
                             signal[0, i, :] = tr.data
 
-                        if channel == 'Z':
+                        if channel == "Z":
                             signal[2, i, :] = tr.data
 
             else:
@@ -186,21 +209,21 @@ class MSEED():
 
     def _load_from_path(self):
         """
-        Retrieves a list of files between two times
+        Retrieves available files between two times
 
         Returns
         -------
-        FILES : list
-            List of available mSEED files
+        files : generator
+            Iterator object of available mSEED files
 
         """
 
         if self.format is None:
-            print('Please specfiy the path_structure - DATA.path_structure')
+            print("Specify the path structure using MSEED.path_structure")
             return
 
         dy = 0
-        FILES = []
+        files = []
         while (self.start_time + (dy * 86400)).julday <= self.end_time.julday:
             now = self.start_time + (dy * 86400)
             for stat in self.stations.tolist():
@@ -209,12 +232,11 @@ class MSEED():
                     month=now.month,
                     jday=str(now.julday).zfill(3),
                     station=stat)
-                files = list(self.MSEED_path.glob(file_format))
-                FILES.extend(files)
+                files = chain(files, self.MSEED_path.glob(file_format))
 
                 dy += 1
 
-        return FILES
+        return files
 
     def _downsample(self, stream, sr):
         """

18a90c5d7ae97d2b4f846ccc88642bc65f0ac9e6
Hemmelig <conor.bacon@gmail.com>
2019-05-18 15:02:12 +0100

    style(C lib): clean up C library
    Stashed deprecated C functions (onset and levinson) and cleaned up the
    related wrappers. Also transferred the omp_set_num_threads(threads);
    call onto the #pragma parallel call in order to remove the message on
    compiling.

--- QMigrate/core/QMigratelib.py
+++ QMigrate/core/QMigratelib.py
@@ -5,13 +5,11 @@ This module requires that 'numpy' is installed in your Python environment.
 This module acts as a wrapper for the C-compiled functions that make up the
 core of the QuakeMigrate package.
 
-TO-DO
------
-
 """
 
 import os
 import pathlib
+
 import numpy as np
 import numpy.ctypeslib as clib
 
@@ -30,147 +28,10 @@ c_i64Pt = clib.ndpointer(dtype=np.int64, flags="C_CONTIGUOUS")
 c_iPt = clib.ndpointer(dtype=np.int32, flags="C_CONTIGUOUS")
 
 p = pathlib.Path(__file__).parents[1] / "lib/QMigrate"
-print(str(p))
 if os.name == 'nt':
     _qmigratelib = clib.load_library(str(p.with_suffix(".dll")), ".")
 else:  # posix
     _qmigratelib = clib.load_library(str(p.with_suffix(".so")), ".")
-
-
-_qmigratelib.onset.argtypes = [c_dPt, c_int, c_int,
-                               c_int, c_int, c_dPt]
-_qmigratelib.onset_mp.argtypes = [c_dPt, c_int, c_int, c_int,
-                                  c_int, c_int, c_dPt]
-
-
-def onset(env, stw, ltw, gap):
-    """
-    Wrapper for the C-compiled onset and onset_mp functions
-
-    Parameters
-    ----------
-    env : 
-
-    stw : 
-
-    ltw : 
-
-    gap : 
-
-    Returns
-    -------
-    out : 
-
-    """
-
-    ntr = env[..., 0].size
-    nsamp = env.shape[-1]
-    out = np.zeros(env.shape, dtype=np.float64)
-    env = np.ascontiguousarray(env, np.float64)
-    if ntr > 1:
-        _qmigratelib.onset_mp(env, ntr, nsamp, int(stw),
-                              int(ltw), int(gap), out)
-        return out
-    else:
-        _qmigratelib.onset(env, nsamp, int(stw),
-                           int(ltw), int(gap), out)
-        return out
-
-
-_qmigratelib.levinson.argtypes = [c_dPt, c_int, c_dPt,
-                                  c_dPt, c_dPt, c_dPt]
-
-_qmigratelib.levinson_mp.argtypes = [c_dPt, c_int, c_int, c_int,
-                                     c_dPt, c_dPt, c_dPt, c_dPt]
-
-
-def levinson(acc, order, return_error=False):
-    """
-    Wrapper for the C-compiled levinson and levinson_mp functions
-
-    Parameters
-    ----------
-    acc : 
-
-    order : 
-
-    return_error : bool
-
-    Returns
-    -------
-    a : 
-
-    k : 
-
-    e : , optional
-
-    """
-
-    acc = np.array(acc, dtype=np.double)
-    if acc.ndim > 1:
-        nsamp = acc.shape[-1]
-        nchan = acc[..., 0].size
-        chan = acc.shape[:-1]
-        a = np.zeros(chan + (order+1,), dtype=np.double)
-        e = np.zeros(chan, dtype=np.double)
-        k = np.zeros(chan + (order,), dtype=np.double)
-        tmp = np.zeros(chan + (order,), dtype=np.double)
-        _qmigratelib.levinson_mp(acc, nchan, nsamp, order, a,
-                                 e, k, tmp)
-    else:
-        nsamp = acc.shape[-1]
-        order = min(order, nsamp-1)
-        a = np.zeros(order+1, dtype=np.double)
-        e = np.zeros(1, dtype=np.double)
-        k = np.zeros(order, dtype=np.double)
-        tmp = np.zeros(order, dtype=np.double)
-        _qmigratelib.levinson(acc, order, a,
-                              e, k, tmp)
-    if return_error:
-        return a, k, e
-    else:
-        return a, k
-
-
-_qmigratelib.nlevinson.argtypes = [c_dPt, c_int,
-                                   c_dPt, c_dPt]
-
-_qmigratelib.nlevinson_mp.argtypes = [c_dPt, c_int, c_int,
-                                      c_dPt, c_dPt]
-
-
-def nlevinson(acc):
-    """
-    Wrapper for the C-compiled nlevinson and nlevinson_mp functions
-
-    Parameters
-    ----------
-    acc : 
-
-    Returns
-    -------
-    a : 
-
-    """
-    acc = np.array(acc, dtype=np.double)
-    if acc.ndim > 1:
-        nsamp = acc.shape[-1]
-        nchan = acc[..., 0].size
-        a = np.zeros(acc.shape, dtype=np.double)
-        tmp = np.zeros(acc.shape, dtype=np.double)
-        _qmigratelib.nlevinson_mp(acc, nchan, nsamp,
-                                  a, tmp)
-    else:
-        nsamp = acc.shape[-1]
-        a = np.zeros(nsamp, dtype=np.double)
-        tmp = np.zeros(nsamp, dtype=np.double)
-        _qmigratelib.nlevinson(acc, nsamp,
-                               a, tmp)
-    return a
-
-
-_qmigratelib.scan4d.argtypes = [c_dPt, c_i32Pt, c_dPt, c_int32, c_int32,
-                                c_int32, c_int32, c_int64, c_int64]
 
 
 def scan(sig, tt, fsmp, lsmp, nsamp, map4d, threads):
@@ -263,43 +124,3 @@ def detect(mmap, dsnr, dind, fsmp, lsmp, threads):
         raise ValueError(msg)
     _qmigratelib.detect4d(mmap, dsnr, dind, c_int32(fsmp), c_int32(lsmp),
                           c_int32(nsamp), c_int64(ncell), c_int64(threads))
-
-
-# _qmigratelib.detect4d_t.argtypes = [c_dPt, c_dPt, c_i64Pt, c_int32,
-#                                    c_int32, c_int32, c_int64, c_int64]
-
-# def detect_t(mmap, dsnr, dind, fsmp, lsmp, threads):
-#     """
-#     Wrapper for the C-compiled detect4d_t function
-
-#     Parameters
-#     ----------
-#     mmap :
-
-#     dsnr : 
-
-#     dind : 
-
-#     fsmp : 
-
-#     lsmp : 
-
-#     threads :
-
-
-#     Raises
-#     ------
-#     ValueError
-#         If the output array size is too small
-
-#     """
-
-#     nsamp = mmap.shape[0]
-#     ncell = np.prod(mmap.shape[1:])
-#     if dsnr.size < nsamp or dind.size < nsamp:
-#         msg = "Output array size is too small, sample count = {}."
-#         msg = msg.format(nsamp)
-#         raise ValueError(msg)
-
-#     _qmigratelib.detect4d_t(mmap, dsnr, dind, c_int32(fsmp), c_int32(lsmp),
-#                            c_int32(nsamp), c_int64(ncell), c_int64(threads))
--- QMigrate/lib/src/QMigrate.c
+++ QMigrate/lib/src/QMigrate.c
@@ -2,12 +2,12 @@
 #include <stdint.h>
 
 #ifndef _OPENMP
-  #define STRING2(x) #x
-  #define STRING(x) STRING2(x)
-  #pragma message (__FILE__ "(" STRING(__LINE__) "): error: This module should be compiled with /openmp on the command line")
+    #define STRING2(x) #x
+    #define STRING(x) STRING2(x)
+    #pragma message (__FILE__ "(" STRING(__LINE__) "): error: This module should be compiled with /openmp on the command line")
 
-  /* Generate a compiler error to stop the build */
-  mustLinkOpenMP
+    /* Generate a compiler error to stop the build */
+    mustLinkOpenMP
 #endif
 
 #if defined(_MSC_VER)
@@ -25,93 +25,61 @@
 #endif
 
 /* Macros for min/max. */
-#define	MIN(a,b) (((a)<(b))?(a):(b))
-#define	MAX(a,b) (((a)>(b))?(a):(b))
-
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
 EXPORT void scan4d(double *sigPt, int32_t *indPt, double *mapPt, int32_t fsmp, int32_t lsmp, int32_t nsamp, int32_t nstation, int64_t ncell, int64_t threads)
 {
-	double	 *stnPt, *stkPt;
-	int32_t  *ttpPt;
-	int32_t  ttp, tend;
-    int32_t  to, tm, st;
-	int64_t  cell;
+    double  *stnPt, *stkPt;
+    int32_t *ttpPt;
+    int32_t ttp, tend;
+    int32_t to, tm, st;
+    int64_t cell;
 
-	omp_set_num_threads(threads);
+    /* omp_set_num_threads(threads); */
 
-	#pragma omp parallel for private(cell,tm,st,stnPt,stkPt,ttpPt,ttp,tend) /* shared(mapPt) */
-	for (cell=0; cell<ncell; cell++)
-	{
-		stkPt = &mapPt[cell * (int64_t) nsamp];
-		ttpPt = &indPt[cell * (int64_t) nstation];
-		// for(tm=0; tm<lsmp-to; tm++)
-		// 	stkPt[tm]  = 0.0;
-		for(st=0; st<nstation; st++)
-		{
-			ttp     = MAX(0,ttpPt[st]);
-			stnPt   = &sigPt[st*(fsmp + lsmp + nsamp) + ttp + fsmp];
-			for(tm=0; tm<nsamp; tm++)
-				stkPt[tm] += stnPt[tm];
-		}
-	}
+    /* shared(mapPt) */
+    #pragma omp parallel for private(cell,tm,st,stnPt,stkPt,ttpPt,ttp,tend) num_threads(threads)
+    for (cell=0; cell<ncell; cell++)
+    {
+        stkPt = &mapPt[cell * (int64_t) nsamp];
+        ttpPt = &indPt[cell * (int64_t) nstation];
+        for(st=0; st<nstation; st++)
+        {
+            ttp   = MAX(0,ttpPt[st]);
+            stnPt = &sigPt[st*(fsmp + lsmp + nsamp) + ttp + fsmp];
+            for(tm=0; tm<nsamp; tm++)
+                stkPt[tm] += stnPt[tm];
+        }
+    }
 }
 
 
 EXPORT void detect4d(double *mapPt, double *snrPt, int64_t *indPt, int32_t fsmp, int32_t lsmp, int32_t nsamp, int64_t ncell, int64_t threads)
 {
-	double	   mv, cv;
-	int32_t  tm;
-	int64_t  cell, ix;
+    double  mv, cv;
+    int32_t tm;
+    int64_t cell, ix;
 
-	/* stack data.... */
+    /* omp_set_num_threads(threads); */
 
-	omp_set_num_threads(threads);
-
-	#pragma omp parallel for private(cell,tm,mv,ix,cv) /* shared(mapPt) */
-	for (tm=fsmp; tm<lsmp; tm++)
-	{
-	    mv = 0.0;
-	    ix = 0;
-	    for (cell=0; cell<ncell; cell++)
-	    {
-	        cv = mapPt[cell * (int64_t) nsamp + (int64_t) tm];
-	        if (cv > mv)
-	        {
-	            mv = cv;
-	            ix = cell;
-	        }
-	    }
-	    snrPt[tm] = mv;
-	    indPt[tm] = ix;
-	}
+    /* stack data.... */
+    /* shared(mapPt) */
+    #pragma omp parallel for private(cell,tm,mv,ix,cv) num_threads(threads)
+    for (tm=fsmp; tm<lsmp; tm++)
+    {
+        mv = 0.0;
+        ix = 0;
+        for (cell=0; cell<ncell; cell++)
+        {
+            cv = mapPt[cell * (int64_t) nsamp + (int64_t) tm];
+            if (cv > mv)
+            {
+                mv = cv;
+                ix = cell;
+            }
+        }
+        snrPt[tm] = mv;
+        indPt[tm] = ix;
+    }
 }
-
-// EXPORT void detect4d_t(double *mapPt, double *snrPt, int64_t *indPt, int32_t fsmp, int32_t lsmp, int32_t nsamp, int64_t ncell, int64_t threads)
-// {
-// 	double	   *rPt, mv, cv;
-// 	int32_t  tm;
-// 	int64_t  cell, ix;
-
-// 	/* stack data.... */
-
-// 	omp_set_num_threads(threads);
-
-// 	#pragma omp parallel for private(cell,tm,mv,ix,cv,rPt)  shared(mapPt) 
-// 	for (tm=fsmp; tm<lsmp; tm++)
-// 	{
-// 	    mv  = 0.0;
-// 	    ix  = 0;
-// 	    rPt = &mapPt[ncell * (int64_t) tm];
-// 	    for (cell=0; cell<ncell; cell++)
-// 	    {
-// 	        cv = rPt[cell];
-// 	        if ( cv > mv)
-// 	        {
-// 	            mv = cv;
-// 	            ix = cell;
-// 	        }
-// 	    }
-// 	    snrPt[tm] = mv;
-// 	    indPt[tm] = ix;
-// 	}
-// }

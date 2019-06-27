7cc1a396b47b824fbeb4255dd27bbc64b5402e16
Hemmelig <conor.bacon@gmail.com>
2019-06-27 15:36:36 +0100

    chore(examples): Update icequake example
    Update the Jupyter notebook and .py script to run the icequake example.
    Confirmed the result is as expected.

--- examples/icequakes/icequake.py
+++ examples/icequakes/icequake.py
@@ -5,7 +5,7 @@
 
 # ## Overview:
 
-# This notebook shows how to run QuakeMigrate for icequake detection, using a 2 minute window of continuous seismicity from Hudson et al (2019). Please refer to this paper for details and justification of the settings used.
+# This notebook shows how to run QuakeMigrate for icequake detection, using a 2 minute window of continuous seismic data from Hudson et al (2019). Please refer to this paper for details and justification of the settings used.
 # 
 # Here, we detail how to:
 # 1. Create a travel-times lookup table for the example seismometer network
@@ -15,42 +15,45 @@
 # 
 # We also provide an outline of some of the key outputs
 
-# In[3]:
+# In[33]:
 
 
 # Import necessary modules:
-import QMigrate.core.model as cmod
-import QMigrate.signal.scan as cscan
-import QMigrate.io.mseed as cmseed
+import QMigrate.core.model as qmod
+import QMigrate.signal.scan as qscan
+import QMigrate.io.data as qdata
+import QMigrate.io.quakeio as qio
+import QMigrate.signal.trigger as qtrigger
 
 
-# In[ ]:
+# In[34]:
 
 
 # Set i/o paths:
-stat_in = "./inputs/stations.txt"
-data_in = "./inputs/mSEED"
-lut_out = "./outputs/lut/icequake.LUT"
-output  = "./outputs/runs/icequake"
+station_file = "./inputs/stations.txt"
+data_in   = "./inputs/mSEED"
+lut_out   = "./outputs/lut/icequake.LUT"
+out_path  = "./outputs/runs"
+run_name  = "icequake_example"
 
 
 # ## 1. Create a travel-times lookup table (LUT)
 
-# In[7]:
+# In[35]:
 
+
+# Read in station information
+stations = qio.stations(station_file)
 
 # Set the parameters for the travel-times lookup table (LUT)
 # Cell count (x,y,z); cell size (x,y,z in metres)
-lut = cmod.LUT(cell_count=[20, 20, 140], cell_size=[100, 100, 20])
+lut = qmod.LUT(stations, cell_count=[20, 20, 140], cell_size=[100, 100, 20])
 lut.lonlat_centre(-17.224, 64.328)
 
 # Set the LUT projection (here we use the Lambert Conformal Conic projection)
-lut.lcc_standard_parallels = (64.32, 64.335)     # Defining the parallels for the projection
-lut.projections(grid_proj_type="LCC")		     # Defining the projection type for the grid
-lut.elevation=1400                               # Defining the elevation of the top of the grid in m 
-
-# Add stations to LUT
-lut.stations(path=stat_in, units="lat_lon_elev") # Defining the station locations within the grid
+lut.lcc_standard_parallels = (64.32, 64.335)
+lut.projections(grid_proj_type="LCC")
+lut.elevation=1400 # Defining the elevation of the top of the grid in m 
 
 # Compute for a homogeneous velocity model
 v_p_homo_model = 3630
@@ -63,100 +66,111 @@ lut.save(lut_out)
 
 # ## 2. Coalesce the seismic energy through time
 
-# In[11]:
+# In[37]:
 
 
 # Create a new instance of the MSEED class and set path structure
-data = cmseed.MSEED(lut_out, HOST_PATH=data_in)
-data.path_structure(path_type="YEAR/JD/*_STATION")
-data.resample = True 
-
+data = qdata.Archive(station_file=station_file, archive_path=data_in)
+data.path_structure(archive_format="YEAR/JD/*_STATION_*")
 
 # Create a new instance of the SeisScan class
-scn = cscan.SeisScan(data, lut_out, output_path=output, output_name="icequake_example")
+scan = qscan.QuakeScan(data, lut_out, output_path=out_path, run_name=run_name)
 
 
-# In[ ]:
+# In[38]:
 
 
 # Set detect parameters
-scn.sampling_rate = 500           # Sampling rate of data, in Hz
-scn.p_bp_filter   = [10, 125, 4]  # The band-pass filter parameters for the P-phase (10 to 125 Hz, with 4th order corners)
-scn.s_bp_filter   = [10, 125, 4]  # The band-pass filter parameters for the P-phase (10 to 125 Hz, with 4th order corners)
-scn.p_onset_win   = [0.01, 0.25]  # Length of the STA and LTA time windows for the P-phase
-scn.s_onset_win   = [0.05, 0.5]   # Length of the STA and LTA time windows for the S-phase
-scn.time_step     = 0.75          # The length of the time-step
-scn.decimate      = [1,1,1]       # Decimation factors in x,y,z (no decimation here)
-scn.n_cores       = 12            # Number of cores/processors to use
+scan.sampling_rate = 500           # Sampling rate of data, in Hz
+scan.p_bp_filter   = [10, 125, 4]  # The band-pass filter parameters for the P-phase (10 to 125 Hz, with 4th order corners)
+scan.s_bp_filter   = [10, 125, 4]  # The band-pass filter parameters for the P-phase (10 to 125 Hz, with 4th order corners)
+scan.p_onset_win   = [0.01, 0.25]  # Length of the STA and LTA time windows for the P-phase
+scan.s_onset_win   = [0.05, 0.5]   # Length of the STA and LTA time windows for the S-phase
+scan.time_step     = 0.75          # The length of the time-step
+scan.decimate      = [1, 1, 1]     # Decimation factors in x,y,z (no decimation here)
+scan.n_cores       = 12            # Number of cores/processors to use
 
 # Defining the start and end times 
 starttime = "2014-06-29T18:41:55.0"
 endtime   = "2014-06-29T18:42:20.0"
 
 
-# In[13]:
+# In[39]:
 
 
 # Run the detect stage to find the coalescence of energy through time:
-scn.detect(starttime, endtime)
+scan.detect(starttime, endtime)
 
 
 # ## 3. Run the trigger stage, to detect and output individual icequakes
 # 
 # nb: We can use the same SeisScan object here because we are not using a different decimation. If running trigger and locate on grids with different levels of decimation, a new SeisScan object must be initialised.
 
-# In[15]:
+# In[41]:
 
 
-# Set trigger parameters:
-scn.detection_threshold   = 1.6   # SNR threshold for the coalescence through time. Will detect an event if the coalescence goes above this for a given timestep
-scn.marginal_window       = 1.0  # The length of the time-step window, + pre and post padding (i.e. 0.75 sec time-step window + 1s padding either side)
-scn.minimum_repeat        = 1.0
-scn.normalise_coalescence = True
+trig = qtrigger.Trigger(out_path, run_name, stations)
+
+trig.normalise_coalescence = True
+trig.marginal_window = 2.75
+trig.minimum_repeat = 6.
+trig.detection_threshold = 1.8
+
+# Run trigger
+trig.trigger(starttime, endtime, savefig=True)
+
+
+# ## 4. Run the locate stage, to relocate triggered events on a less decimated grid
+
+# In[42]:
+
+
+# Set locate parameters:
+scan.marginal_window = 2.75
 
 # Turn on plotting features
-scn.plot_coal_video      = False
-scn.plot_coal_grid       = False
-scn.plot_coal_picture    = True
-scn.plot_coal_trace      = False
+scan.plot_coal_video      = False
+scan.plot_coal_grid       = False
+scan.plot_coal_picture    = True
+scan.plot_coal_trace      = False
 
 
-# In[16]:
+# In[43]:
 
-
-# Run the trigger stage to find any events
-scn.trigger(starttime, endtime)
 
 # Run the locate stage to determine the location of any triggered events
-scn.locate(starttime, endtime)
+scan.locate(starttime, endtime)
 
 
-# ## 4. Some of the key outputs 
+# ## 4. Some of the key outputs
+
+# In[48]:
+
 
 # Show the .event file, containing event origin time and location:
-icequake_event_fname = "outputs/runs/icequake/icequake_example_20140629184210336000.event"
+icequake_event_fname = "./outputs/runs/icequake_example/events/20140629184210330000.event"
 with open(icequake_event_fname) as f:
     lines = f.readlines()
 for line in lines:
     print(line)
 
 
-# In[23]:
+# In[49]:
 
 
 # Show the .stn file, containing station time picks:
-icequake_stn_fname = "outputs/runs/icequake/icequake_example_20140629184210336000.stn"
+icequake_stn_fname = "outputs/runs/icequake_example/picks/20140629184210330000.picks"
 with open(icequake_stn_fname) as f:
     lines = f.readlines()
 for line in lines:
     print(line)
 
 
-# In[4]:
+# In[50]:
 
 
 # Show the coalescence pdf file, containing event origin time and location:
-icequake_coal_image_fname = "outputs/runs/icequake/icequake_example_20140629184210336000_EventLocationError.pdf"
+icequake_coal_image_fname = "outputs/runs/icequake_example/summaries/icequake_example_20140629184210330000_EventSummary.pdf"
 from IPython.display import IFrame # For plotting pdf
 IFrame(icequake_coal_image_fname, width=800, height=400) # Plot pdf
 

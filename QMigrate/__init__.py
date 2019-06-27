ce496f0467a46907fb1d91da3aa643985c383ed4
Hemmelig <conor.bacon@gmail.com>
2019-06-27 12:24:41 +0100

    VERSION 1.0 RELEASE
    Update __init__.py to version 1.0

--- QMigrate/__init__.py
+++ QMigrate/__init__.py
@@ -1,5 +1,5 @@
 name = "QuakeMigrate"
-__version__ = "0.2"
+__version__ = "1.0"
 __description__ = "QuakeMigrate - Microearthquake detection and location software"
 __license__ = "GPL v3.0"
 __author__ = "Jonathan Smith, Tom Winder, Tom Hudson, Conor Bacon, Tim Greenfield and Julian Drew,"

f9d9369d53bf43c4c88e9b906e8f94b012c760d3
twinder36 <40000578+twinder36@users.noreply.github.com>
2020-06-27 21:47:11 +0100

    docs(io/amplitudes): Update docstrings.

--- QMigrate/io/amplitudes.py
+++ QMigrate/io/amplitudes.py
@@ -7,14 +7,22 @@ Module to handle input/output of .amps files.
 
 def write_amplitudes(run, amplitudes, event):
     """
-    Write amplitude values to a new .amps file.
+    Write amplitude results to a new .amps file. This includes amplitude
+    measurements, and the magnitude estimates derived from them (with station
+    correction terms appied, if provided).
 
     Parameters
     ----------
     run : `QMigrate.io.Run` object
         Light class encapsulating i/o path information for a given run.
-    amplitudes :
-
+    amplitudes : `pandas.DataFrame` object
+        P- and S-wave amplitude measurements for each component of each
+        station in the station file, and individual local magnitude estimates
+        derived from them.
+        Columns = ["epi_dist", "z_dist", "P_amp", "P_freq", "P_time",
+                   "S_amp", "S_freq", "S_time", "Noise_amp", "is_picked", "ML",
+                   "ML_Err"]
+        Index = Trace ID (see `obspy.Trace` object property 'id')
     event : `QMigrate.io.Event` object
         Light class encapsulating signal, onset, and location information for a
         given event.

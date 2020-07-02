9771a9e3f2ce80f89be8ae44eaf9431e1bd592c6
Conor Bacon <conor.bacon@gmail.com>
2020-07-02 23:48:46 +0100

    refactor: Streamline helper module for loading shared library
    Remove helper function that identifies the compiler/platform specific
    shared library extension. This was only needed to support older versions
    of Python for which the "EXT_SUFFIX" config variable was not used.
    
    Added additional detail providing more information if no shared library
    can be found/loaded using ctypes.CDLL.

--- QMigrate/core/libnames.py
+++ QMigrate/core/libnames.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Helpers to get library names.
+Helper to load compiled C library.
 
-Edited for our use from obspy.core.util.libnames
 """
 
 import ctypes
@@ -10,45 +9,10 @@ from distutils import sysconfig
 import pathlib
 
 
-def _get_lib_path(libdir, libname):
-    """
-    Grab architecture/Python version specific library filename.
-
-    Parameters
-    ----------
-    libdir : `pathlib.Path` object
-        Path to library to load.
-    libname : str
-        Name of library to be loaded.
-
-    Returns
-    -------
-    libpath : `pathlib.Path` object
-        Path to library with architecture/Python version specific extension.
-
-    """
-
-    # Get extension suffix defined by Python for current platform
-    ext_suffix = sysconfig.get_config_var("EXT_SUFFIX")
-    # In principle "EXT_SUFFIX" is what we want.
-    # "SO" seems to be deprecated on newer python
-    # but: older Python seems to have empty "EXT_SUFFIX", so we fall back
-    if not ext_suffix:
-        try:
-            ext_suffix = sysconfig.get_config_var("SO")
-        except Exception as e:
-            print(e.msg)
-            pass
-    if ext_suffix:
-        libpath = (libdir / libname).with_suffix(ext_suffix)
-
-    return libpath
-
-
 def _load_cdll(name):
     """
-    Helper function to load a shared library built during installation
-    with ctypes.
+    Helper function to load an extension built using setuptools/distutils
+    Extension.
 
     Parameters
     ----------
@@ -63,12 +27,15 @@ def _load_cdll(name):
     """
 
     # our custom defined part of the extension file name
-    libdir = pathlib.Path(__file__).parent / "src"
-    libpath = _get_lib_path(libdir, name)
+    libpath = pathlib.Path(__file__).parent / "src" / name
+    lib = libpath.with_suffix(sysconfig.get_config_var("EXT_SUFFIX"))
     try:
-        cdll = ctypes.CDLL(libpath)
+        cdll = ctypes.CDLL(lib)
     except Exception as e:
-        msg = f"Could not load shared library '{libpath.name}'.\n\n {e}"
+        msg = (f"Could not load extension library '{libpath.name}'.\n\n{e}\n"
+               "If you have chosen to install from a clone of the github "
+               "repository, please ensure you have run setup.py, which will "
+               "compile and install the C library.")
         raise ImportError(msg)
 
     return cdll

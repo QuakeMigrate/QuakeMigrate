fix: Ensure precision is returned
Ensure precision (position of first significant figure wrt the decimal point) by
adding a factor of -1.

QMigrate/lut/lut.py
@@ -302,7 +302,7 @@ class Grid3D:
 
         """
 
-        return [int(np.format_float_scientific(axis).split("e")[1]) for axis
+        return [-int(np.format_float_scientific(axis).split("e")[1]) for axis
                 in np.subtract(*self.index2coord([[0, 0, 0], [1, 1, 1]]))]
 
     @property
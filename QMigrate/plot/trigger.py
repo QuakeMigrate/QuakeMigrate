b047c309b3fec2113ce884f022c330b01c2e1e2a
Conor Bacon <conor.bacon@gmail.com>
2020-07-11 19:11:47 +0100

    refactor: Stylistic changes to trigger summary plot
    Make some stylistic changes the the trigger summary plot. In this process,
    the `trigger_summary` function was split into logical sections, which were
    assigned to subroutines.
    
    Changes include:
    
    - Add time span information to summary text plot
    - Make use of the custom DateFormatter introduced in 29012bf
    - Move the colorbar to a less prominent position in the plot
    - Zoomed in on the y axis of the station availability plot to only show
    a window +10% of the max / -20% of the min station availability.
    - Added bounding-boxes to the data trace plots.
    
    NOTE: Will need to manually merge the changes introduced in #80 to the naming
    of `minimum_repeat`.

--- QMigrate/plot/trigger.py
+++ QMigrate/plot/trigger.py
@@ -52,7 +52,7 @@ def trigger_summary(events, starttime, endtime, run, marginal_window,
         Estimate of time error over which to marginalise the coalescence.
     minimum_repeat : float
         Minimum time interval between triggered events.
-    detection_threshold : float
+    detection_threshold : array-like
         Coalescence value above which to trigger events.
     normalise_coalescence : bool
         If True, use coalescence normalised by the average background noise.
@@ -72,111 +72,48 @@ def trigger_summary(events, starttime, endtime, run, marginal_window,
     dt = pd.to_datetime(data["DT"].astype(str))
 
     fig = plt.figure(figsize=(30, 15))
+    gs = (9, 18)
 
     # Create plot axes, ordering: [COA, COA_N, AVAIL, XY, XZ, YZ]
     for row in [0, 3, 6]:
-        spec = GridSpec(9, 18).new_subplotspec((row, 9), colspan=9, rowspan=3)
-        ax = fig.add_subplot(spec)
+        ax = plt.subplot2grid(gs, (row, 8), colspan=10, rowspan=3, fig=fig)
         ax.set_xlim([starttime.datetime, endtime.datetime])
-    lut.plot(fig, (9, 18))
+
+    # --- Plot LUT, coalescence traces, and station availability ---
+    lut.plot(fig, gs)
     axes = fig.axes
-
-    # Share coalescence and availability axes
-    for ax in axes[0:2]:
+    for ax in axes[:2]:
         ax.get_shared_x_axes().join(ax, axes[2])
-
-    lab = "Maximum coalescence"
-    axes[0].plot(dt, data.COA, c="k", lw=0.01, label=lab, alpha=0.8,
-                 zorder=10)
-    axes[1].plot(dt, data.COA_N, c="k", lw=0.01, alpha=0.8,
-                 zorder=10)
-
+    # for ax, data, label in zip(fig.axes[:2], )
+    _plot_coalescence(axes[0], dt, data.COA, "Maximum coalescence")
+    _plot_coalescence(axes[1], dt, data.COA_N,
+                      "Normalised maximum coalescence")
     try:
         logging.info("\n\t    Reading in .StationAvailability...")
         availability = read_availability(run, starttime, endtime)
-        available = availability.sum(axis=1).astype(int)
-        times = list(pd.to_datetime(available.index))
-
-        # Handle last step
-        available = available.values
-        available = np.append(available, [available[-1]])
-        times.append(endtime.datetime)
-
-        axes[2].step(times, available, c="green", where="post")
-        axes[2].set_ylim([0, max(available) + 1])
+        _plot_station_availability(axes[2], availability, endtime)
     except util.NoStationAvailabilityDataException as e:
         logging.info(e)
 
+    # --- Plot event scatter on LUT and windows on coalescence traces ---
     if events is not None:
-        for i, event in events.iterrows():
-            lab1 = "Minimum repeat window" if i == 0 else ""
-            lab2 = "Marginal window" if i == 0 else ""
-            lab3 = "Triggered event" if i == 0 else ""
+        _plot_event_windows(axes[:2], events, marginal_window)
+        _plot_event_scatter(fig, events)
 
-            min_dt = event["MinTime"].datetime
-            max_dt = event["MaxTime"].datetime
-            mw_stt = (event["CoaTime"] - marginal_window).datetime
-            mw_end = (event["CoaTime"] + marginal_window).datetime
-            for ax in axes[:2]:
-                ax.axvspan(min_dt, mw_stt, label=lab1, alpha=0.2, color="#F03B20")
-                ax.axvspan(mw_end, max_dt, alpha=0.2, color="#F03B20")
-                ax.axvspan(mw_stt, mw_end, label=lab2, alpha=0.2, color="#3182BD")
-                ax.axvline(event["CoaTime"].datetime, label=lab3, lw=0.01,
-                           alpha=0.4)
-
-    axes[0].text(0.01, 0.925, "Maximum coalescence", ha="left", va="center",
-                 transform=axes[0].transAxes, fontsize=18)
-    axes[0].set_ylabel("Coalescence", fontsize=14)
-    axes[1].text(0.01, 0.925, "Normalised maximum coalescence", ha="left",
-                 va="center", transform=axes[1].transAxes, fontsize=18)
-    axes[1].set_ylabel("Normalised coalescence", fontsize=14)
-    axes[2].text(0.01, 0.925, "Station availability", ha="left", va="center",
-                 transform=axes[2].transAxes, fontsize=18)
-    axes[2].set_ylabel("Available stations", fontsize=14)
-    axes[2].set_xlabel("DateTime", fontsize=14)
-
-    if events is not None:
+        # Add trigger threshold to the correct coalescence trace
         ax_i = 1 if normalise_coalescence else 0
         axes[ax_i].step(dt, detection_threshold, where="mid", c="g",
                         label="Detection threshold")
 
-        # Get bounds for cmap
-        vmin, vmax = events["COA_V"].min(), events["COA_V"].max()
-
-        # Plotting the scatter of the earthquake locations
-        x, y, z = events["COA_X"], events["COA_Y"], events["COA_Z"]
-        c = events["COA_V"]
-        sc = axes[3].scatter(x, y, s=50, c=c, cmap="viridis", vmin=vmin,
-                             vmax=vmax)
-        axes[4].scatter(x, z, s=50, c=c, cmap="viridis", vmin=vmin, vmax=vmax)
-        axes[5].scatter(z, y, s=50, c=c, cmap="viridis", vmin=vmin, vmax=vmax)
-
-        # --- Add colourbar ---
-        spec = GridSpec(9, 18).new_subplotspec((2, 7), colspan=1, rowspan=5)
-        cax = fig.add_subplot(spec)
-        cax.set_axis_off()
-        cbar = fig.colorbar(sc, ax=cax, orientation="vertical", fraction=0.4)
-        cbar.ax.set_ylabel("Peak coalescence value", rotation=90, fontsize=14)
-
     # --- Write summary information ---
-    text = plt.subplot2grid((9, 18), (0, 0), colspan=8, rowspan=2, fig=fig)
-    if len(set(detection_threshold)) == 1:
-        threshold = f"{detection_threshold[0]} (static)"
-    else:
-        threshold = "dynamic"
-    text.text(0.5, 0.75, f"Trigger threshold: {threshold}", ha="center",
-              va="center", fontsize=22)
-    text.text(0.5, 0.6, f"Marginal window: {marginal_window} s", ha="center",
-              va="center", fontsize=22)
-    text.text(0.5, 0.45, f"Minimum repeat time: {minimum_repeat} s",
-              ha="center", va="center", fontsize=22)
-    trig = "normalised coalescence" if normalise_coalescence else "coalescence"
-    count = len(events) if events is not None else 0
-    text.text(0.5, 0.2, (f"Triggered {count} event(s) on the {trig}"
-                         " trace."), ha="center", va="center", fontsize=22)
-    text.set_axis_off()
+    text = plt.subplot2grid(gs, (0, 0), colspan=8, rowspan=2, fig=fig)
+    st, et = [t.strftime("%Y-%m-%d %H:%M:%S") for t in (starttime, endtime)]
+    text.text(0.42, 0.8, f"{st}  -  {et}", fontsize=20, fontweight="bold",
+              ha="center")
+    _plot_text_summary(text, events, detection_threshold, marginal_window,
+                       minimum_repeat, normalise_coalescence)
 
-    axes[0].legend(loc=1, fontsize=14)
+    fig.axes[0].legend(loc=1, fontsize=14)
     fig.tight_layout(pad=1, h_pad=0)
     plt.subplots_adjust(wspace=0.3, hspace=0.3)
 
@@ -189,3 +126,187 @@ def trigger_summary(events, starttime, endtime, run, marginal_window,
         plt.savefig(str(file))
     else:
         plt.show()
+
+
+def _plot_station_availability(ax, availability, endtime):
+    """
+    Utility function to handle all aspects of plotting the station
+    availability.
+
+    Parameters
+    ----------
+    ax : `~matplotlib.Axes` object
+        Axes on which to plot the waveform gather.
+    availability : `~pandas.DataFrame` object
+        Dataframe containing the availability of stations through time.
+    endtime : `~obspy.UTCDateTime`
+        End time of trigger run.
+
+    """
+
+    available = availability.sum(axis=1).astype(int)
+    times = list(pd.to_datetime(available.index))
+
+    # Handle last step
+    available = available.values
+    available = np.append(available, [available[-1]])
+    times.append(endtime.datetime)
+    ax.step(times, available, c="green", where="post")
+
+    _add_plot_tag(ax, "Station availability")
+    ax.set_ylim([int(min(available)*0.8), int(max(available)*1.1)])
+    ax.set_yticks(range(int(min(available)*0.8), int(max(available)*1.1)+1))
+    ax.xaxis.set_major_formatter(util.DateFormatter("%H:%M:%S.{ms}", 2))
+    ax.set_xlabel("DateTime", fontsize=14)
+    ax.set_ylabel("Available stations", fontsize=14)
+
+
+def _plot_coalescence(ax, dt, data, label):
+    """
+    Utility function to bring plotting of coalescence into one place.
+
+    Parameters
+    ----------
+    ax : `~matplotlib.Axes` object
+        Axes on which to plot the coalescence traces.
+    dt : array-like
+        Timestamps of the coalescence data.
+    data : array-like
+        Coalescence data to plot.
+    label : str
+        y-axis label.
+
+    """
+
+    if label == "Maximum coalescence":
+        ax.plot(dt, data, c="k", lw=0.01, label=label, alpha=0.8, zorder=10)
+    else:
+        ax.plot(dt, data, c="k", lw=0.01, alpha=0.8, zorder=10)
+    _add_plot_tag(ax, label)
+    ax.set_ylabel(label, fontsize=14)
+    ax.xaxis.set_major_formatter(util.DateFormatter("%H:%M:%S.{ms}", 2))
+
+
+def _add_plot_tag(ax, tag):
+    """
+    Utility function to plot tags on data traces.
+
+    Parameters
+    ----------
+    ax : `~matplotlib.Axes` object
+        Axes on which to plot the tag.
+    tag : str
+        Text to go in the tag.
+
+    """
+
+    ax.text(0.01, 0.925, tag, ha="left", va="center", transform=ax.transAxes,
+            bbox=dict(boxstyle="round", fc="w", alpha=0.8), fontsize=18)
+
+
+def _plot_event_scatter(fig, events):
+    """
+    Utility function for plotting the triggered events as a scatter on the
+    LUT cross-sections.
+
+    Parameters
+    ----------
+    fig : `~matplotlib.Figure` object
+        Figure containing axes on which to plot event scatter.
+    events : `~pandas.DataFrame` object
+        Dataframe of triggered events.
+
+    """
+
+    # Get bounds for cmap
+    vmin, vmax = events["COA_V"].min(), events["COA_V"].max()
+
+    # Plotting the scatter of the earthquake locations
+    x, y, z = events["COA_X"], events["COA_Y"], events["COA_Z"]
+    c = events["COA_V"]
+    sc = fig.axes[3].scatter(x, y, s=50, c=c, vmin=vmin, vmax=vmax)
+    fig.axes[4].scatter(x, z, s=50, c=c, vmin=vmin, vmax=vmax)
+    fig.axes[5].scatter(z, y, s=50, c=c, vmin=vmin, vmax=vmax)
+
+    # --- Add colourbar ---
+    cax = plt.subplot2grid((9, 18), (7, 5), colspan=1, rowspan=2, fig=fig)
+    cax.set_axis_off()
+    cb = fig.colorbar(sc, ax=cax, orientation="vertical", fraction=0.4)
+    cb.ax.set_ylabel("Peak coalescence value", rotation=90, fontsize=14)
+
+
+def _plot_event_windows(axes, events, marginal_window):
+    """
+    Utility function for plotting the marginal event window and minimum event
+    interval for triggered events.
+
+    Parameters
+    ----------
+    axes : list of `~matplotlib.Axes` objects
+        Axes on which to plot the event windows.
+    events : `~pandas.DataFrame` object
+        Dataframe of triggered events.
+    marginal_window : float
+        Estimate of time error over which to marginalise the coalescence.
+
+    """
+
+    for i, event in events.iterrows():
+        lab1 = "Minimum repeat window" if i == 0 else ""
+        lab2 = "Marginal window" if i == 0 else ""
+        lab3 = "Triggered event" if i == 0 else ""
+
+        min_dt = event["MinTime"].datetime
+        max_dt = event["MaxTime"].datetime
+        mw_stt = (event["CoaTime"] - marginal_window).datetime
+        mw_end = (event["CoaTime"] + marginal_window).datetime
+        for ax in axes:
+            ax.axvspan(min_dt, mw_stt, label=lab1, alpha=0.2, color="#F03B20")
+            ax.axvspan(mw_end, max_dt, alpha=0.2, color="#F03B20")
+            ax.axvspan(mw_stt, mw_end, label=lab2, alpha=0.2, color="#3182BD")
+            ax.axvline(event["CoaTime"].datetime, label=lab3, lw=0.01,
+                       alpha=0.4)
+
+
+def _plot_text_summary(ax, events, threshold, marginal_window, minimum_repeat,
+                       normalise_coalescence):
+    """
+    Utility function to plot the event summary information.
+
+    Parameters
+    ----------
+    ax : `~matplotlib.Axes` object
+        Axes on which to plot the text summary.
+    events : `~pandas.DataFrame` object
+        DataFrame of triggered events.
+    threshold : array-like
+        Coalescence value above which to trigger events.
+    marginal_window : float
+        Estimate of time error over which to marginalise the coalescence.
+    minimum_repeat : float
+        Minimum time interval between triggered events.
+    normalise_coalescence : bool
+        If True, use coalescence normalised by the average background noise.
+
+    """
+
+    # Get threshold info
+    if len(set(threshold)) == 1:
+        threshold = f"{threshold[0]} (static)"
+    else:
+        threshold = "dynamic"
+
+    # Get trigger on and event count info
+    trig = "normalised coalescence" if normalise_coalescence else "coalescence"
+    count = len(events) if events is not None else 0
+
+    with plt.rc_context({"font.size": 18}):
+        ax.text(0.45, 0.65, "Trigger threshold:", ha="right", va="center")
+        ax.text(0.47, 0.65, f"{threshold}", ha="left", va="center")
+        ax.text(0.45, 0.5, "Marginal window:", ha="right", va="center")
+        ax.text(0.47, 0.5, f"{marginal_window} s", ha="left", va="center")
+        ax.text(0.45, 0.35, "Minimum repeat time:", ha="right", va="center")
+        ax.text(0.47, 0.35, f"{minimum_repeat} s", ha="left", va="center")
+        ax.text(0.42, 0.15, f"Triggered {count} event(s) on the {trig} trace.",
+                ha="center", va="center")
+    ax.set_axis_off()

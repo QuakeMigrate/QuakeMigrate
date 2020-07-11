63837e764430c0734144f8da7a8bf7d2c9c6fbb7
Conor Bacon <conor.bacon@gmail.com>
2020-07-11 02:07:47 +0100

    chore: Make viridis the default colormap
    Make viridis the default colormap on the import of QMigrate.plot

--- QMigrate/plot/__init__.py
+++ QMigrate/plot/__init__.py
@@ -8,7 +8,12 @@ in QuakeMigrate, including:
 
 """
 
+import matplotlib.pyplot as plt
+
 from .event import event_summary  # NOQA
 from .phase_picks import pick_summary  # NOQA
 from .trigger import trigger_summary  # NOQA
 from .amplitudes import amplitudes_summary  # NOQA
+
+
+plt.viridis()

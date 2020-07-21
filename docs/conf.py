76d70f881fe589ffcdb270360a47065c5e5a0b0e
Hemmelig <conor.bacon@gmail.com>
2020-07-21 18:25:26 +0100

    docs: Update logo
    Update the logo being used in the documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-html_logo = 'img/QMLogo.png'
+html_logo = 'img/QMlogoBig.png'
 
 # -- Options for HTML output -------------------------------------------------
 

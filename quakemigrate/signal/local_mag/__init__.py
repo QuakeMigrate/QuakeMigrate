6e314099c07702cb2148c8dcf855c577b3aa5568
TomWinder <tom.winder@esc.cam.ac.uk>
2021-01-14 00:20:11 +0000

    chore(stickler): Formatting improvements suggested by Stickler

--- quakemigrate/signal/local_mag/__init__.py
+++ quakemigrate/signal/local_mag/__init__.py
@@ -4,9 +4,10 @@ The :mod:`quakemigrate.local_mag` extension module handles the calculation of
 local magnitudes from Wood-Anderson simulated waveforms.
 
 .. warning:: The local_mag modules are an ongoing work in progress. We hope to\
- continue to extend their functionality, which may result in some API changes. \
-If you have comments or suggestions, please contact the QuakeMigrate \
-developers at: quakemigrate.developers@gmail.com , or submit an issue on GitHub.
+ continue to extend their functionality, which may result in some API changes.\
+ If you have comments or suggestions, please contact the QuakeMigrate \
+developers at: quakemigrate.developers@gmail.com , or submit an issue on \
+GitHub.
 
 :copyright:
     2020 - 2021, QuakeMigrate developers.
--- quakemigrate/signal/onsets/__init__.py
+++ quakemigrate/signal/onsets/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-The :mod:`quakemigrate.onsets` module handles the generation of Onset functions.
-The default method uses the ratio between the short-term and long-term
-averages of the signal amplitude.
+The :mod:`quakemigrate.onsets` module handles the generation of Onset
+functions. The default method uses the ratio between the short-term and
+long-term averages of the signal amplitude.
 
 Feel free to contribute more Onset function options!
 

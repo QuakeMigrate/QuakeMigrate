5ea220899691f607c9ed06d8d0a0e9211838d759
TomWinder <tom.winder@esc.cam.ac.uk>
2024-03-07 17:47:46 +0000

    :bug: Single client request for all stations
    Previously data was downloaded from each station sequentially in a
    loop; this instead makes use of the ability to list multiple stations
    in a single request. This should speed things up a little, and
    hopefully address #172

--- examples/Volcanotectonic_Iceland/get_dike_intrusion_data.py
+++ examples/Volcanotectonic_Iceland/get_dike_intrusion_data.py
@@ -29,17 +29,16 @@ endtime = UTCDateTime("2014-236T00:15:00")
 
 #  --- Read in station file ---
 stations = read_stations(station_file)
+stations_string = ",".join(stations["Name"])
 
 # --- Download instrument response inventory ---
-inv = Inventory()
-for station in stations["Name"]:
-    inv += client.get_stations(
-        network=network,
-        station=station,
-        starttime=starttime,
-        endtime=endtime,
-        level="response",
-    )
+inv = client.get_stations(
+    network=network,
+    station=stations_string,
+    starttime=starttime,
+    endtime=endtime,
+    level="response",
+)
 inv.write(response_file, format="STATIONXML")
 
 # --- Make directories to store waveform data ---
@@ -47,20 +46,23 @@ waveform_path = data_path / str(starttime.year) / f"{starttime.julday:03d}"
 waveform_path.mkdir(parents=True, exist_ok=True)
 
 # --- Download waveform data ---
+print(f"Downloading waveform data from {datacentre}")
+st = client.get_waveforms(
+    network=network,
+    station=stations_string,
+    location="*",
+    channel="*H*",
+    starttime=starttime,
+    endtime=endtime,
+)
+st.merge(method=-1)
+
+# --- Write waveform data to disk ---
 for station in stations["Name"]:
-    print(f"Downloading waveform data for station {station} from {datacentre}")
-    st = client.get_waveforms(
-        network=network,
-        station=station,
-        location="*",
-        channel="*H*",
-        starttime=starttime,
-        endtime=endtime,
-    )
-    st.merge(method=-1)
+    print(f"Writing data from station {station} to disk")
     for comp in ["E", "N", "Z"]:
         try:
-            st_comp = st.select(component=comp)
+            st_comp = st.select(station=station, component=comp)
             st_comp.write(str(waveform_path / f"{station}_{comp}.m"), format="MSEED")
         except IndexError:
             pass

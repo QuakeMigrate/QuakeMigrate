7788af74d60409d79c7440a084d23faa7c50daef
TomWinder <tom.winder@esc.cam.ac.uk>
2024-09-23 20:20:13 +0100

    :wrench: Switch VT_Iceland example to MassDownloader
    Switch the VT_Iceland example to use the obspy MassDownloader functions
    for obtaining data from IRIS/EarthScope, instead of the simple client
    interface. This is faster and cleaner.
    
    The only slight downside is that if you re-run the MassDownloader with
    the same target directory, it will generally not re-download the data if
    you just slightly tweak the start and end times, as the test for whether
    the data has been downloaded already is not very sophisticated. The
    solution is to clear the target directories first.
    
    Note also I have left `mdl.download.threads_per_client=3` (the default)
    but this might need adjusting in case it causes any re-occurrence of #172.

--- examples/Volcanotectonic_Iceland/get_dike_intrusion_data.py
+++ examples/Volcanotectonic_Iceland/get_dike_intrusion_data.py
@@ -8,61 +8,64 @@ for the Iceland dike intrusion example.
 
 import pathlib
 
-from obspy import UTCDateTime, Inventory
-from obspy.clients.fdsn import Client
+from obspy import UTCDateTime
+from obspy.clients.fdsn.mass_downloader import (
+    GlobalDomain,
+    Restrictions,
+    MassDownloader,
+)
 
 from quakemigrate.io import read_stations
 
 # --- i/o paths ---
 station_file = "./inputs/iceland_stations.txt"
 data_path = pathlib.Path("./inputs/mSEED")
-response_file = "./inputs/Z7_dataless.xml"
+stationxml_storage = "./inputs/DATALESS"
+
+
+# --- Define directory structure for storing waveform data ---
+def get_mseed_storage(network, station, location, channel, starttime, endtime):
+    fname = (
+        data_path
+        / f"{starttime.year}"
+        / f"{starttime.julday:03d}"
+        / f"{station}_{channel[2]}.m"
+    ).as_posix()
+
+    return fname
+
 
 # --- Set network code & client ---
 network = "Z7"
-datacentre = "IRIS"
-client = Client(datacentre)
+datacentres = ["IRIS"]
+# global domain (specifying network and stations instead)
+domain = GlobalDomain()
 
 # --- Set time period over which download data ---
 starttime = UTCDateTime("2014-236T00:00:00")
 endtime = UTCDateTime("2014-236T00:15:00")
 
-#  --- Read in station file ---
+# --- Read in station file ---
 stations = read_stations(station_file)
 stations_string = ",".join(stations["Name"])
 
-# --- Download instrument response inventory ---
-inv = client.get_stations(
-    network=network,
-    station=stations_string,
+# --- Set up request ---
+restrictions = Restrictions(
     starttime=starttime,
     endtime=endtime,
-    level="response",
+    chunklength_in_sec=86400,
+    network=network,
+    station=stations_string,
+    channel_priorities=["HH[ZNE]", "BH[ZNE]"],
+    minimum_interstation_distance_in_m=0,
 )
-inv.write(response_file, format="STATIONXML")
-
-# --- Make directories to store waveform data ---
-waveform_path = data_path / str(starttime.year) / f"{starttime.julday:03d}"
-waveform_path.mkdir(parents=True, exist_ok=True)
 
 # --- Download waveform data ---
-print(f"Downloading waveform data from {datacentre}")
-st = client.get_waveforms(
-    network=network,
-    station=stations_string,
-    location="*",
-    channel="*H*",
-    starttime=starttime,
-    endtime=endtime,
+mdl = MassDownloader(providers=datacentres)
+mdl.download(
+    domain,
+    restrictions,
+    threads_per_client=4,
+    mseed_storage=get_mseed_storage,
+    stationxml_storage=stationxml_storage,
 )
-st.merge(method=-1)
-
-# --- Write waveform data to disk ---
-for station in stations["Name"]:
-    print(f"Writing data from station {station} to disk")
-    for comp in ["E", "N", "Z"]:
-        try:
-            st_comp = st.select(station=station, component=comp)
-            st_comp.write(str(waveform_path / f"{station}_{comp}.m"), format="MSEED")
-        except IndexError:
-            pass

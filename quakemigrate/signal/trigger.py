QuakeMigrate/QuakeMigrate

commit 989c498399b8fd73b1093a9f9dd54df8901d0114

:bug: set default trigger threshold to None
Update the default trigger threshold when instantiating a new Trigger
object to be `None` if no keyword argument is supplied, NOT `static`.
This now means a `InvalidTriggerThresholdMethodException` should
correctly be raised.

quakemigrate/signal/trigger.py
@@ -205,7 +205,7 @@ class Trigger:
         self.run.logger(kwargs.get("log", False))
 
         # --- Grab Trigger parameters or set defaults ---
-        self.threshold_method = kwargs.get("threshold_method", "static")
+        self.threshold_method = kwargs.get("threshold_method")
         if self.threshold_method == "static":
             self.static_threshold = kwargs.get("static_threshold", 1.5)
         elif self.threshold_method == "mad":

commit 3b703c260df0fc88d49e6718b77723a1105ab980

:bug: Fix setter for trigger threshold method
This resolves the bug introduced in 989c498, where if the threshold
method was not set at Instantiation, the following if statements related
to the threshold method would fail.

The threshold method property/setter is modified to check the chosen
method is valid and apply the correct error message if not.

In order to ensure a value is always set for the relevant parameters
(mad_window_length, etc etc) they are now all set via kwargs.get()
irrespective of the threshold method chosen. This may cause overlap if
more methods are added in future (and is a bit inelegant), but works ok
for now.

Tested with valid and invalid method strings, with and without the
relevant threshold parameters set in the run script. Also with 'dynamic'
to ensure the FutureWarning is still printed correctly.

quakemigrate/signal/trigger.py
@@ -205,17 +205,13 @@ class Trigger:
         self.run.logger(kwargs.get("log", False))
 
         # --- Grab Trigger parameters or set defaults ---
-        self.threshold_method = kwargs.get("threshold_method")
-        if self.threshold_method == "static":
-            self.static_threshold = kwargs.get("static_threshold", 1.5)
-        elif self.threshold_method == "mad":
-            self.mad_window_length = kwargs.get("mad_window_length", 3600.0)
-            self.mad_multiplier = kwargs.get("mad_multiplier", 8.0)
-        elif self.threshold_method == "median_ratio":
-            self.median_window_length = kwargs.get("median_window_length", 3600.0)
-            self.median_multiplier = kwargs.get("median_multiplier", 1.2)
-        else:
-            raise util.InvalidTriggerThresholdMethodException
+        self.threshold_method = kwargs.get("threshold_method", "static")
+        self.static_threshold = kwargs.get("static_threshold", 1.5)
+        self.mad_window_length = kwargs.get("mad_window_length", 3600.0)
+        self.mad_multiplier = kwargs.get("mad_multiplier", 8.0)
+        self.median_window_length = kwargs.get("median_window_length", 3600.0)
+        self.median_multiplier = kwargs.get("median_multiplier", 1.2)
+
         self.marginal_window = kwargs.get("marginal_window", 2.0)
         self.min_event_interval = kwargs.get("min_event_interval", 4.0)
         if kwargs.get("minimum_repeat"):
@@ -692,6 +688,26 @@ class Trigger:
         else:
             self._min_event_interval = value
 
+    @property
+    def threshold_method(self):
+        """Get and set the threshold method."""
+
+        return self._threshold_method
+
+    @threshold_method.setter
+    def threshold_method(self, value):
+        if value in ["static", "mad", "median_ratio"]:
+            self._threshold_method = value
+        elif value == "dynamic":
+            # DEPRECATED
+            print(
+                "FutureWarning: This threshold method has been renamed - continuing.\n"
+                "To remove this message, change:\n\t'dynamic' -> 'mad'"
+            )
+            self._threshold_method = "mad"
+        else:
+            raise util.InvalidTriggerThresholdMethodException
+
     # --- Deprecation/Future handling ---
     @property
     def minimum_repeat(self):
@@ -709,23 +725,3 @@ class Trigger:
             "\t'minimum_repeat' -> 'min_event_interval'"
         )
         self._min_event_interval = value
-
-    # --- Deprecation/Future handling ---
-    @property
-    def threshold_method(self):
-        """Handle deprecated 'dynamic' threshold method."""
-        return self._threshold_method
-
-    @threshold_method.setter
-    def threshold_method(self, value):
-        """Handle deprecated threshold_method kwarg / attribute and print warning."""
-
-        if value == "dynamic":
-            # DEPRECATED
-            print(
-                "FutureWarning: This threshold method has been renamed - continuing.\n"
-                "To remove this message, change:\n\t'dynamic' -> 'mad'"
-            )
-            self._threshold_method = "mad"
-        else:
-            self._threshold_method = value

quakemigrate/util.py
@@ -1070,7 +1070,9 @@ class InvalidTriggerThresholdMethodException(Exception):
     """
 
     def __init__(self):
-        super().__init__("Only 'static' or 'dynamic' thresholds are supported.")
+        super().__init__(
+            "Only 'static', 'mad' or 'median_ratio' thresholds are " "supported."
+        )
 
 
 class InvalidPickThresholdMethodException(Exception):